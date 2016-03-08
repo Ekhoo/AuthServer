cd40ef5a97c7ba952510d1c142b2d288234269ab
Lucas Ortis <lucas.ortis.external@watchever.com>
2016-03-08 12:12:39 +0100

    Protect routes with a token

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var User = require("./domain/model/User");
 var mongoose = require("mongoose");
 var config = require('./config');
 var morgan = require('morgan');
+var jwt = require('jsonwebtoken');
 var createUserAction = require('./application/createUserAction');
 var getUserAction = require('./application/getUserAction');
 var updateUserAction = require('./application/updateUserAction');
@@ -17,8 +18,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : true}));
 app.use(morgan('dev'));
 
-/*** User ***/
 router.route("/user/register").post(createUserAction);
+
+router.use(function (request, response, next) {
+    var token = request.headers['x-access-token'];
+
+    if (token) {
+        jwt.verify(token, config.secret, function (error, decoded) {
+            if (error) {
+                response.json({
+                    error: true,
+                    message: 'Token validation failed'
+                });
+            } else {
+                request.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        response.status(403);
+        response.json({
+            error: true,
+            message: 'No token provided.'
+        });
+    }
+});
+
 router.route("/user/:id")
     .get(getUserAction)
     .put(updateUserAction)
